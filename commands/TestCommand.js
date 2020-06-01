7d5092146ead1ae406061bd20d6f6310f1ff9bbe
Mike Erickson <mike.erickson@codedungeon.io>
2020-06-01 11:42:53 -0700

    chore: fix linting issues

--- commands/TestCommand.js
+++ commands/TestCommand.js
@@ -3,20 +3,11 @@ module.exports = {
   description: 'This is description',
   flags: {
     name: { aliases: ['n'], description: 'command name', required: false },
-    description: { aliases: ['d'], description: 'command description', required: false },
+    description: { aliases: ['d'], description: 'command description', required: false }
   },
   execute(cli) {
-		// leave this line intact, it will process arguments, applying defaults where applicable
-    cli.arguments = cli.setDefaultFlags(cli, this.flags)
-
-		// all arguments can be accesses off the `cli.arguments` variable
-		cli.print.information('Name: ', cli.arguments.name)
-		cli.print.information('Description: ', cli.arguments.description)
-
-		// this can be useful to easily print all arguments
-    cli.print.important('Arguments:     ' + JSON.stringify(cli.arguments).replace(/,/gi, ', '))
-
-		// example processing command
-		cli.print.info(`Hello ${cli.arguments.name}`)
+    // leave this line intact, it will process arguments, applying defaults where applicable
+    cli.arguments = cli.setDefaultFlags(cli, this.flags) // all arguments can be accesses off the `cli.arguments` variabl    		cli.print.information('Name: ', cli.arguments.name    		cli.print.information('Description: ', cli.arguments.description)    		// this can be useful to easily print all arguments
+    cli.print.important('Arguments:     ' + JSON.stringify(cli.arguments).replace(/,/gi, ', ')) // example processing comman    		cli.print.info(`Hello ${cli.arguments.name}`)
   }
 }
