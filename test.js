41320d615f36fd3978b17d19da99cb76b14bb4fa
Mike Erickson <codedungeon@gmail.com>
2021-02-13 16:15:32 -0800

    remove extraneous test file

--- test.js
+++
@@ -1,7 +0,0 @@
-const { filesystem, system, colors, print } = require('./src/gunner')
-
-const src = filesystem.path.join(__dirname, '..')
-
-print.log(colors.green(`${src}\n`))
-
-print.success(`${src}\n`, 'SUCCESS')
